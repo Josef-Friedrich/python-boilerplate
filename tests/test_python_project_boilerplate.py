97181e667774eefc5eb2a763b448f177b280f847
Josef Friedrich <josef@friedrich.rocks>
2022-07-16 21:09:47 +0200

    Fix tests

--- tests/test_python_project_boilerplate.py
+++ tests/test_python_project_boilerplate.py
@@ -8,3 +8,6 @@ class PythonProjectBoilerplateTest(unittest.TestCase):
     def test_version(self):
         self.assertEqual(
             python_project_boilerplate.__version__, '0.1.0')
+
+    def test_print(self):
+        print('use nose2 --output-buffer to capture the printed test')
