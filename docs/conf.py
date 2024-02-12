973fabc88f8ebef0f2664787a9f522be983985e9
Josef Friedrich <josef@friedrich.rocks>
2024-02-12 19:32:36 +0100

    Improve Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -4,28 +4,29 @@
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-import sphinx_rtd_theme
-
-import python_boilerplate
+import sphinx_rtd_theme  # type: ignore
+from importlib.metadata import version as get_version
+from datetime import datetime
 
 html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme_path: list[str] = [sphinx_rtd_theme.get_html_theme_path()]
 
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.todo",
     "sphinx.ext.viewcode",
+    "sphinxcontrib.jquery",
 ]
 
 project = "Python project boilerplate"
-copyright = "2022, Josef Friedrich"
+copyright: str = f"2022-{datetime.now().year}, Josef Friedrich"
 author = "Josef Friedrich"
 
-version = python_boilerplate.__version__
-release = python_boilerplate.__version__
+version: str = get_version("python_boilerplate")
+release: str = get_version("python_boilerplate")
 language = "en"
 
-exclude_patterns = ["_build"]
+exclude_patterns: list[str] = ["_build"]
 
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autodoc_default_options
 autodoc_default_options = {
